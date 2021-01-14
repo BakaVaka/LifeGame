414f1e1d9fb5599ba079b84d094cff313ff13e03
BakaVaka <hotwetloli2008@gmail.com>
2021-01-14 19:41:31 +0300

    Первый и единственный коммит

--- LifeGame/LifeGame.cs
+++ LifeGame/LifeGame.cs
@@ -2,6 +2,7 @@
 {
     using SFML.Graphics;
     using System;
+    using System.Linq;
 
     public class LifeGame : SfmlApplication
     {
@@ -12,19 +13,14 @@
         private RectangleShape _worldShape = new RectangleShape(new SFML.System.Vector2f(width,height));
         private Image _image = new Image(width, height);
         private bool[] _cells = new bool[width * height];
-        private bool[] _nextCellBuffer = new bool[width * height];
+        private bool[] _nextCellsBuffer = new bool[width * height];
         public LifeGame() : base(_title, width, height)
         {
             _worldShape.Texture = new Texture(_image);
-            InitRandom(95);
+            InitRandom(25);
         }
 
-        private void InitRandom(uint fillFactor)
-        {
-            if (fillFactor > 100) fillFactor = 100;
-            for (int i = 0; i < _cells.Length; i++)
-                _cells[i] = fillFactor - rnd.Next(0, 100) >= 0 ;
-        }
+
 
         protected override void Draw(RenderWindow window)
         {
@@ -33,8 +29,76 @@
 
         protected override void Update(float elapsedTime)
         {
+            //Т.к. клеток дохуя, не будем никак использовать время
+
+            for(int i = 0; i < height; i++)
+            {
+                for(int j = 0; j < width; j++)
+                {
+                    var index = GetCellInex(i, j);
+                    _nextCellsBuffer[index] = LifeGameRulles(i, j);
+
+                    if(_nextCellsBuffer[index])
+                    {
+                        _image.SetPixel((uint)i, (uint)j, Color.White);
+                    }
+                    else
+                    {
+                        _image.SetPixel((uint)i, (uint)j, Color.Black);
+                    }
+
+                }
+            }
 
             _worldShape.Texture.Update(_image);
+            Array.Copy(_nextCellsBuffer, _cells, _nextCellsBuffer.Length);
+        }
+
+        private bool LifeGameRulles(int i, int j)
+        {
+            var n = CountNeighbors(i, j);
+            if(GetCell(i,j))
+            {
+                if (n == 2 || n == 3)
+                    return true;
+            }
+            else
+            {
+                if (n == 3)
+                    return true;
+            }
+            return false;
+        }
+
+        private bool GetCell(int row, int col) => _cells[GetCellInex(row, col)];
+        private int GetCellInex(int row, int col) => (int)(row * width + col);
+        private void InitRandom(uint fillFactor)
+        {
+            if (fillFactor > 100) fillFactor = 100;
+            for (int i = 0; i < _cells.Length; i++)
+                _cells[i] = fillFactor - rnd.Next(0, 100) >= 0;
+        }
+        private int CountNeighbors(int row, int col)
+        {
+            int startRow = row - 1 < 0 ? 0 : row - 1;
+            int endRow = row + 1 > height - 1 ? (int)height - 1 : row + 1;
+
+            int startCol = col - 1 < 0 ? 0 : col - 1;
+            int endCol = col + 1 > width - 1 ? (int)width - 1 : col + 1;
+
+            int n = 0;
+            for(int i = startRow; i <= endRow; i++)
+            {
+                for(int j = startCol; j <= endCol; j++)
+                {
+                    if (i == row && j == col) continue;
+                    if (GetCell(i, j))
+                    {
+                        n++;
+                    }
+                }
+            }
+            return n;                    
         }
     }
 }
